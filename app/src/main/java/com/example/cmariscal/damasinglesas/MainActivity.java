196248a51912c7f41969206e85c01410e6051e60
marzo23 <cristymaris@hotmail.com>
2018-06-12 05:31:58 -0500

    Dama´s movement corrected

--- app/src/main/java/com/example/cmariscal/damasinglesas/MainActivity.java
+++ app/src/main/java/com/example/cmariscal/damasinglesas/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         //Casillas =casillas;
         TextView playButton = new TextView(this);
         playButton.setWidth(ancho/numCasillas);
+        playButton.setHeight(ancho/numCasillas);
         playButton.setBackgroundResource(R.drawable.retry);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,8 +78,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        TextView passButton = new TextView(this);
+        passButton.setWidth(ancho/numCasillas);
+        passButton.setHeight(ancho/numCasillas);
+        passButton.setBackgroundResource(R.drawable.arrow);
+        passButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetStates(casillas);
+                turnoMsg();
+            }
+        });
+
         TableRow buttonRow = new TableRow(this);
         buttonRow.addView(playButton);
+        buttonRow.addView(passButton);
         tabla.addView(buttonRow);
 
         setContentView(tabla);
@@ -86,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
     int I = -1;
     int J = -1;
     int turno = 0;
+
 
     public void setCasillaRelations(final Casilla[][] casillas){
         for(int i = 0; i<casillas.length; i++) {
@@ -106,8 +121,12 @@ public class MainActivity extends AppCompatActivity {
                 casillas[i][j].view.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if(validateIsOnlySelected(casillas, view) && (I<0 || J<0))
+
+                        if(validateIsOnlySelected(casillas, view) && (I<0 || J<0)){
+
                             resetStates(casillas);
+                            turnoMsg();
+                        }
                         else
                             selectCasilla(casillas[I][J]);
                     }
@@ -117,37 +136,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void resetStates(Casilla[][] casillas){
-
-        boolean flag = false;
+        int team1Count = 0;
+        int team2Count = 0;
         for(int i = 0; i<casillas.length; i++) {
             for (int j = 0; j < casillas.length; j++) {
                 casillas[i][j].unselect();
                 if(!casillas[i][j].getIsEmpty()){
-                    if(I>-1 && J>-1){
-                        if(casillas[I][J].teamNumber==casillas[i][j].teamNumber){
-                            flag = true;
-                            I = i;
-                            J = j;
-                        }else
-                            flag = false;
-                    }
+                    if(casillas[i][j].teamNumber==1)
+                        team1Count++;
+                    else if(casillas[i][j].teamNumber==2)
+                        team2Count++;
                 }
             }
         }
-        if(!flag){
-            I = -1;
-            J = -1;
-        }else
-            win(casillas[I][J]);
+        if(team1Count==0 || team2Count==0)
+            win(team1Count==0?1:2);
 
     }
 
-    public void win(Casilla casilla){
-        Toast.makeText(casilla.view.getContext(), "GANÓ EQUIPO: "+casilla.teamNumber, Toast.LENGTH_SHORT).show();
+    public void turnoMsg(){
+        if(turno == 1)
+            turno = 2;
+        else if(turno == 2)
+            turno = 1;
+        Toast.makeText(this, "TURNO EQUIPO: "+(turno==1?"Blanco":"Rojo"), Toast.LENGTH_SHORT).show();
+    }
+
+    public void win(int team){
+        Toast.makeText(this, "GANÓ EQUIPO: "+(team==1?"Blanco":"Rojo"), Toast.LENGTH_SHORT).show();
     }
 
     public void selectCasilla(Casilla casilla){
-        casilla.select();
+        if(turno==casilla.teamNumber)
+            casilla.select();
+        else{
+            Toast.makeText(this, "Movimiento erroneo, turno equipo: "+(turno==1?"Blanco":"Rojo"), Toast.LENGTH_SHORT).show();
+        }
+        //casilla.select();
     }
 
     public boolean validateIsOnlySelected(Casilla[][] casillas, View view){
@@ -162,11 +187,8 @@ public class MainActivity extends AppCompatActivity {
                     if(currentSelected!=null && casillas[i][j].getIsValidMovement() && !casillas[i][j].getIsSelected()){
                         I = -1;
                         J = -1;
-                        currentSelected.move(casillas[i][j]);
-                        flag = true;
+                        flag = currentSelected.move(casillas[i][j]);
                     }
-                    //else if(!casillas[i][j].getIsValidMovement())
-                        //casillas[i][j].unselect();
                     else {
                         I = i;
                         J = j;
@@ -174,11 +196,7 @@ public class MainActivity extends AppCompatActivity {
 
                 }else {
                     if(casillas[i][j].getIsValidMovement() && currentSelected!=null){
-                        Casilla aux = casillas[i][j].getPreviousJump();
-                        while (aux.getPreviousJump()!=null){
-                            aux = aux.getPreviousJump();
-                        }
-                        if(!aux.equals(currentSelected))
+                        if(!currentSelected.allowedMovements.contains(casillas[i][j]))
                             casillas[i][j].unselect();
                     }
                     if(casillas[i][j].getIsSelected()) {
@@ -191,6 +209,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public  void setNewGame(Casilla[][] casillas){
+        turno = 1;
         for(int i = 0; i<casillas.length-3; i++){
             for(int j = 0; j<casillas.length; j++){
                 if(casillas[i][j].isBlack && i<3){
@@ -203,9 +222,6 @@ public class MainActivity extends AppCompatActivity {
                     casillas[i][j].teamNumber = 0;
                     casillas[i][j].setIsEmplty(true);
                 }
-                /*else {
-                    casillas[casillas.length-1-i][casillas.length-1-j].setIsEmplty(false);
-                }*/
             }
         }
     }
@@ -220,6 +236,7 @@ class Casilla{
     private boolean isEmplty;
     public boolean isBlack;
     public boolean isDama;
+    public List<Casilla> allowedMovements;
     private boolean isValidMovement;
     private boolean isSelected;
     private Casilla previousJump;
@@ -233,6 +250,7 @@ class Casilla{
 
     public Casilla(TextView view, final boolean isEmplty, boolean isBlack, int x, int y, int size, int numCasillas){
         casillaList = new ArrayList<Casilla>();
+        allowedMovements = new ArrayList<Casilla>();
         this.view = view;
         this.isBlack = isBlack;
         this.isEmplty = isEmplty;
@@ -293,37 +311,17 @@ class Casilla{
                 view.setBackgroundResource(R.drawable.black_selected);
             else if(teamNumber==2)
                 view.setBackgroundResource(R.drawable.black_selected2);
-            Log.d("k","START SET MOVMENTS***************************************************");
-            setMovements(this);
+            setMovements();
 
         }
     }
 
-    public void move(Casilla destiny){
-        if(destiny.isEmplty){
+    public boolean move(Casilla destiny){
+        if(destiny.isEmplty && this.allowedMovements.contains(destiny) && destiny.isValidMovement){
             if((this.teamNumber==2 && destiny.y == 0) || (this.teamNumber==1 && destiny.y == numCasillas-1))
                 destiny.isDama = true;
             if(destiny.previousJump!=null)
-            if(destiny.previousJump.equals(this)){
-                if(teamNumber==1)
-                    destiny.view.setBackgroundResource(R.drawable.black_full);
-                else if(teamNumber==2)
-                    destiny.view.setBackgroundResource(R.drawable.black_full2);
-                destiny.isEmplty = false;
-                destiny.teamNumber = teamNumber;
-                this.remove();
-            }else {
-                Casilla aux = destiny.previousJump;
-                while(aux.previousJump!=null){
-                    if(aux.teamNumber!=this.teamNumber)// && aux.teamNumber!=0)
-                        aux.remove();
-                    else{
-                        Toast.makeText(view.getContext(), "Error, brincaste una ficha de tu color.", Toast.LENGTH_SHORT).show();
-                        break;
-                    }
-                    aux = aux.previousJump;
-                }
-                if(aux.equals(this)){
+                if(destiny.previousJump.equals(this)){
                     if(teamNumber==1)
                         destiny.view.setBackgroundResource(R.drawable.black_full);
                     else if(teamNumber==2)
@@ -331,10 +329,35 @@ class Casilla{
                     destiny.isEmplty = false;
                     destiny.teamNumber = teamNumber;
                     this.remove();
+                    return true;
+                }else {
+                    Casilla aux = destiny.previousJump;
+                    while(aux.previousJump!=null){
+                        if(aux.teamNumber!=this.teamNumber)// && aux.teamNumber!=0)
+                            aux.remove();
+                        else{
+                            Toast.makeText(view.getContext(), "Error, brincaste una ficha de tu color.", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        aux = aux.previousJump;
+                    }
+                    if(aux.equals(this)){
+                        if(teamNumber==1)
+                            destiny.view.setBackgroundResource(R.drawable.black_full);
+                        else if(teamNumber==2)
+                            destiny.view.setBackgroundResource(R.drawable.black_full2);
+                        destiny.isEmplty = false;
+                        destiny.teamNumber = teamNumber;
+                        this.remove();
+                        for (int j = 0; j<allowedMovements.size(); j++)
+                            allowedMovements.get(j).unselect();
+                        allowedMovements = new ArrayList<Casilla>();
+                        return true;
+                    }
                 }
-            }
-        }
-
+        }else
+            Toast.makeText(this.view.getContext(), "Movimiento Erroneo.", Toast.LENGTH_SHORT).show();
+        return false;
     }
 
     public void remove(){
@@ -351,44 +374,34 @@ class Casilla{
         }
     }
 
-    private boolean setMovements(Casilla casilla){
-
-        Log.d("k","ACTUAL : x: "+casilla.x+" y: "+casilla.y + " Color: "+casilla.teamNumber);
-        Log.d("k", "PREVIOUS : x: "+(casilla.previousJump==null?"null":casilla.previousJump.x)
-                +" y: "+(casilla.previousJump==null?"null":casilla.previousJump.y)
-                +" Color: "+(casilla.previousJump==null?"null":casilla.previousJump.teamNumber));
-        boolean flag = false;
+    private void setMovements(){
+        /*boolean flag = false;
         for (int i = 0; i<casilla.casillaList.size(); i++){
             boolean isAdvancing = false;
             Casilla aux = null;
             if(casilla.previousJump==null){
                 isAdvancing = (casilla.casillaList.get(i).y>casilla.y && casilla.teamNumber ==1)
                         || (casilla.casillaList.get(i).y<casilla.y && casilla.teamNumber ==2)
-                        || casilla.isDama;
+                        || this.isDama;
             }else {
-                aux = casilla.previousJump;
-                while (aux.previousJump!=null){
-                    aux = aux.previousJump;
-                }
+                aux = this;
                 isAdvancing = (casilla.casillaList.get(i).y>casilla.y && aux.teamNumber ==1)
                         || (casilla.casillaList.get(i).y<casilla.y && aux.teamNumber ==2)
-                        || casilla.isDama;
+                        || this.isDama;
             }
 
             if(casilla.casillaList.get(i).isEmplty && isAdvancing) {
                 if(casilla.previousJump==null){
                     casilla.casillaList.get(i).isValidMovement = true;
                     casilla.casillaList.get(i).previousJump = casilla;
-                    Log.d("k", "setRelation: \nNextX: "+ casilla.casillaList.get(i).x+" NextY: "+casilla.casillaList.get(i).y);
-                    Log.d("k", "setRelation: \nActualX: "+ casilla.x+" ActualY: "+casilla.y);
+                    this.allowedMovements.add(casilla.casillaList.get(i));
                     casilla.casillaList.get(i).view.setBackgroundResource(R.drawable.allowed);
                     flag = true;
                 }else {
-                    if(!casilla.isEmplty && casilla.casillaList.get(i).x!= casilla.previousJump.x){
+                    if(!casilla.isEmplty && casilla.casillaList.get(i).x!= casilla.previousJump.x && casilla.casillaList.get(i).y!= casilla.previousJump.y){
                         casilla.casillaList.get(i).isValidMovement = true;
                         casilla.casillaList.get(i).previousJump = casilla;
-                        Log.d("k", "setRelation: \nNextX: "+ casilla.casillaList.get(i).x+" NextY: "+casilla.casillaList.get(i).y);
-                        Log.d("k", "setRelation: \nActualX: "+ casilla.x+" ActualY: "+casilla.y);
+                        this.allowedMovements.add(casilla.casillaList.get(i));
                         casilla.casillaList.get(i).view.setBackgroundResource(R.drawable.allowed);
                         flag = true;
                         if (flag)
@@ -403,12 +416,9 @@ class Casilla{
                     tn = aux.teamNumber;
                 }else if(!casilla.isEmplty && casilla.previousJump==null)
                     tn = casilla.teamNumber;
-                //else if(!casilla.isEmplty && casilla.previousJump!=null)
-                    //tn = casilla.previousJump.teamNumber;
                 if(tn > 0 && casilla.casillaList.get(i).teamNumber != tn){
                     casilla.casillaList.get(i).previousJump = casilla;
-                    Log.d("k", "setRelation: \nNextX: "+ casilla.casillaList.get(i).x+" NextY: "+casilla.casillaList.get(i).y);
-                    Log.d("k", "setRelation: \nActualX: "+ casilla.x+" ActualY: "+casilla.y);
+                    //this.allowedMovements.add(casilla.casillaList.get(i));
                     if (flag)
                         setMovements(casilla.casillaList.get(i));
                     else
@@ -417,7 +427,73 @@ class Casilla{
 
             }
         }
-        return flag;
+        return flag;//*/
+        if(this.isDama)
+            damaMovements(this);
+        else
+            normalMovements(this);
+    }
+
+    private void damaMovements(Casilla casilla){
+        for (int i = 0; i<casilla.casillaList.size(); i++){
+            casilla.casillaList.get(i).previousJump = casilla;
+            if(casilla.casillaList.get(i).isEmplty)
+                simpleJump(casilla.casillaList.get(i));
+            else
+                recursiveMovement(casilla.casillaList.get(i));
+        }
+    }
+
+    private void simpleJump(Casilla destiny){
+        destiny.isValidMovement = true;
+        destiny.previousJump = this;
+        this.allowedMovements.add(destiny);
+        if(this.isDama)
+            destiny.isDama = true;
+        destiny.view.setBackgroundResource(R.drawable.allowed);
+    }
+
+    private void normalMovements(Casilla casilla){
+        for (int i = 0; i<casilla.casillaList.size(); i++){
+            boolean isAdvancing =  (casilla.casillaList.get(i).y>casilla.y && this.teamNumber ==1)
+                                || (casilla.casillaList.get(i).y<casilla.y && this.teamNumber ==2);
+            if(!isAdvancing)
+                continue;
+            casilla.casillaList.get(i).previousJump = casilla;
+            if(casilla.casillaList.get(i).isEmplty)
+                simpleJump(casilla.casillaList.get(i));
+            else
+                recursiveMovement(casilla.casillaList.get(i));
+        }
+    }
+
+    private void recursiveJump(Casilla from, Casilla destiny){
+        if(destiny.x!= from.previousJump.x && destiny.y!= from.previousJump.y){
+            destiny.isValidMovement = true;
+            destiny.previousJump = from;
+            this.allowedMovements.add(destiny);
+            destiny.view.setBackgroundResource(R.drawable.allowed);
+            if(this.isDama)
+                destiny.isDama = true;
+            for (int i = 0; i<destiny.casillaList.size(); i++){
+                boolean isAdvancing =  (destiny.casillaList.get(i).y>destiny.y && this.teamNumber ==1)
+                                    || (destiny.casillaList.get(i).y<destiny.y && this.teamNumber ==2)
+                                    || this.isDama;
+                if(!destiny.casillaList.get(i).isEmplty && isAdvancing && !destiny.casillaList.get(i).equals(from)) {
+                    destiny.casillaList.get(i).previousJump = destiny;
+                    recursiveMovement(destiny.casillaList.get(i));
+                }
+            }
+        }
+    }
+
+    private void recursiveMovement(Casilla casilla){
+        if(!casilla.isEmplty && casilla.teamNumber!=this.teamNumber){
+            for (int i = 0; i<casilla.casillaList.size(); i++){
+                if(casilla.casillaList.get(i).isEmplty)
+                    recursiveJump(casilla, casilla.casillaList.get(i));
+            }
+        }
     }
 
     private void removeChip(Casilla casilla){
